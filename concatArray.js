ed01c70dfddf93489822ce87e47ff00c2567b3f3
teerasak <teerasak.jiar@kmutt.ac.th>
2022-09-11 19:05:56 +0700

    Success

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,38 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  let array3;
+  if (array1 === null || array1 === undefined) {
+    if (array2 === null) {
+      array3 = undefined;
+    } else if (array2 === undefined) {
+      array3 = undefined;
+    } else {
+      array3 = array2;
+    }
+  } else if (array2 === null || array2 === undefined) {
+    if (array1 === null) {
+      array3 = undefined;
+    } else if (array1 === undefined) {
+      array3 = undefined;
+    } else {
+      array3 = array1;
+    }
+  } else if (array1.length === 0) {
+    if (array2.length === 0) {
+      array3 = [];
+    } else {
+      array3 = array2;
+    } 
+  } else if (array2.length === 0) {
+    if (array1.length === 0) {
+      array3 = [];
+    } else {
+      array3 = array1;
+    }
+  } else {
+    array3 = array1.concat(array2);
+  }
+  return array3;
 }
 module.exports = concatArray
